a7f0fa13467b10429fda6917c1646e1868ff5bc3
Applin <lapplin.olivier@gmail.com>
2020-02-29 21:20:02 -0500

    forgot day2-2

---
+++ 2015/day2-2.hs
@@ -0,0 +1,21 @@
+{-
+  Advent of code : Day 2 - 2
+  Meausre the lenght of ribbon required to to wrap all present
+-}
+module Day2_2 where
+import Day2
+import Data.List
+
+smallestSidePerimeter :: Box -> Int
+smallestSidePerimeter (l, w, h) = (l*w*h) + (x+x+y+y) where
+  [x, y, xs] = sort [l, w, h]
+
+perimeterLine :: String -> Int
+perimeterLine = smallestSidePerimeter . parseLine
+
+main :: IO Int
+main = do
+  file <- readFile "day2-data.txt"
+  let sizes = map perimeterLine (lines file)
+  return $ sum sizes
+  
