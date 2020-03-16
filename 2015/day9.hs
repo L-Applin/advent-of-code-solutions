4b8cb2a43bafdea1526d43ab9aef301e8400cfde
Applin <lapplin.olivier@gmail.com>
2020-03-15 22:46:19 -0400

    2015: day-9 1-2

---
+++ 2015/day9.hs
@@ -0,0 +1,46 @@
+import Data.List
+import Data.Maybe
+import Data.List.HT
+import Data.List.Split
+import qualified Data.Map.Strict as Map
+
+
+type City = String
+-- cities :: [City]
+-- cities = ["Q", "M", "T"]
+
+type Path = ((City, City), Int)
+dist :: [Path]
+dist = [(("Q","M"),10), (("M","T"),20), (("Q","T"),25)]
+
+
+distances :: [Path] -> [City]  -> Int
+distances paths cities = sum
+                         $ mapAdjacent (\a -> \b -> cityLookup (a, b) paths) cities 
+
+cityLookup :: (City, City) -> [Path] ->  Int
+cityLookup (a, b) paths = case (lookup (a, b) paths) of
+                            Just i -> i
+                            Nothing -> case (lookup (b, a) paths) of
+                                         Just j -> j
+                                         Nothing -> error "city not found"
+
+parse :: String -> ([City], Path)
+parse str = ([c1, c2], ((c1, c2), read d)) where
+  words = splitOn " " str
+  c1 = words!!0
+  c2 = words!!2
+  d = words!!4
+
+
+
+main :: IO ()
+main = do 
+  file <- readFile "day9-data.txt"
+  let ln = lines file
+  let input = map parse ln
+  let cities = nub $ foldl (\cs -> \(c, _) -> cs ++ c) [] input
+  let paths = map snd input
+  let dist = map (distances paths) (permutations cities)
+  print $ "max: " ++ (show $ maximum dist) ++ ", min: " ++ (show $ minimum dist)
+
