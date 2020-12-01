b3e0dd197e285b3e6a0f2bd6671b118be9262c24
Applin <lapplin.olivier@gmail.com>
2020-12-01 17:28:04 -0500

    day 1 - 2020

---
+++ 2020/day1.hs
@@ -0,0 +1,20 @@
+import Data.List
+
+pairwise :: [a] -> [(a, a)]
+pairwise l = [ (x,y) | (x:ys) <- tails l, y <- ys ]
+
+isSum :: Num a => Eq a => a -> (a, a) -> Bool
+isSum i (a ,b) = i == (a + b) 
+
+twoEntriesThatSumUpTo :: Num a => Eq a => a -> [a] -> (a, a)
+twoEntriesThatSumUpTo target xs = head $ filter (isSum target) $ pairwise xs
+
+findProduct :: Num a => Eq a => a -> [a] -> a
+findProduct i xs = a * b where
+  (a, b) = twoEntriesThatSumUpTo i xs
+
+main :: IO Int 
+main = do
+  content <- readFile "input.txt"
+  let allLines = map read $ lines content 
+  return $ findProduct 2020 allLines
