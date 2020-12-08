c10b2ef3c6115727208736484b755f6d9e73e46c
Applin <lapplin.olivier@gmail.com>
2020-12-08 00:38:21 -0500

    day5-2 2020

--- 2020/day5.hs
+++ 2020/day5.hs
@@ -1,6 +1,10 @@
+import Data.List
+
 type Seat = (Int, Int)
 data Split = Upper | Lower
 
+allSeats :: [Seat]
+allSeats = [ (x,y) | x <- [0..127], y <- [0..7] ]
 
 splitSeat :: Split -> [Int] -> [Int]
 splitSeat Upper xs = drop (length xs `div` 2) xs 
@@ -28,7 +32,16 @@ toSeat s = (row, col) where
 seatId :: Seat -> Int
 seatId (a,b) = a*8 + b
 
+getContent :: IO [String]
+getContent = fmap lines $ readFile "day5-data.txt"
+
 main :: IO ()
 main = do
-  allLines <- fmap lines $ readFile "day5-data.txt"
+  allLines <- getContent 
   print $ foldl max 0 $ map (seatId . toSeat) allLines
+
+
+main_2 :: IO ()
+main_2 = do
+  allLines <- getContent
+  print $ allSeats \\ map toSeat allLines
